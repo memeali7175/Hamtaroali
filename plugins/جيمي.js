f8c9884776c77caa6a0785ffc1d5caaa4ae78135
aliquteenah <145256032+aliquteenah@users.noreply.github.com>
2024-07-27 10:56:29 +0300

    Update جيمي.js

--- plugins/جيمي.js
+++ plugins/جيمي.js
@@ -2,7 +2,7 @@ import Gemini from "gemini-ai"
 
 let handler = async (m, { conn, args }) => {
   const text = args.join` `;
-  if (!text) return conn.reply(m.chat, '*معك Gemini الذكاء الاصطناعي من شركة غوغل* يمكنك استخدامي عبر كتابة مثلا هكذا :\n\n*.gemini* من هو ابن القيم', m);
+  if (!text) return conn.reply(m.chat, '*معك Gemini الذكاء الاصطناعي من شركة غوغل* يمكنك استخدامي عبر كتابة مثلا هكذا :\n\n*.جيمي* من هو ابن القيم', m);
 const gemini = new Gemini("AIzaSyBlmjS5n8VOT10r2vzM8N7JFiZw_Y6eHJw") //GET APIKEY IN HERE https://makersuite.google.com/app/apikey
          const chat = gemini.createChat();
          let a = await chat.ask(text)
